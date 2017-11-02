5e5eb4fbcd739ca5a7d9235d18724d63ceb48809
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-02 19:24:11 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-  array.join(", ")
+case array.length
+  when 1
+    array.join
+  when 2
+    array.join("and ")
+  else
+    count = 0
+    new_string = ""
+    while count < array.length - 2 do
+      new_string << "#{array[count]}, "
+      count ++
+    end
+    new_string << "and #{array[array.length-1]}"
+    new_string
+end
 end
