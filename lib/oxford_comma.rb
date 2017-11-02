833233efcaaed940b2d96206841b7465b0e313f5
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-02 19:28:21 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,13 +5,6 @@ case array.length
   when 2
     array.join("and ")
   else
-    count = 0
-    new_string = ""
-    while count < array.length - 2 do
-      new_string << "#{array[count]}, "
-      count ++
-    end
-    new_string << "and #{array[array.length-1]}"
-    new_string
+    
   end
 end
